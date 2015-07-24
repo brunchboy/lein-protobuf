e61d2c8f21ad0739e144258b262dbbc57ca4497d
James Elliott <james.elliott@singlewire.com>
2015-07-23 19:30:49 -0500

    Non-snapshot version while awaiting pull request.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.brunchboy/lein-protobuf "0.4.3-SNAPSHOT"
+(defproject org.clojars.brunchboy/lein-protobuf "0.4.3"
   :description "Leiningen plugin for compiling protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/flatland/protobuf"
+  :url "https://github.com/brunchboy/lein-protobuf"
   :dependencies [[fs "1.2.0"]
                  [conch "0.2.0"]
                  [leinjacker "0.4.0"]]
